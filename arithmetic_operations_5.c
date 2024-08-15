103f440d8181312792bba675701d83eed5fe6887
AlexanderErmolov <amkof03@mail.ru>
2024-08-15 10:46:49 +0300

    Add files via upload

---
+++ arithmetic_operations_5.c
@@ -0,0 +1,21 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(void)
+{
+    int a = 5;
+    int b = 2;
+    int c = a % b;
+    printf("a=%d \n", a);
+    printf("b=%d \n", b);
+    printf("c = a %% b = %d", c);
+ 
+    return 0;
+}
