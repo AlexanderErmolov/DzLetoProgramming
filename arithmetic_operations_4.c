9f34a579dd481beaa5390a7b75ee69d658e32547
AlexanderErmolov <amkof03@mail.ru>
2024-08-15 10:43:18 +0300

    Add files via upload

---
+++ arithmetic_operations_4.c
@@ -0,0 +1,22 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(void)
+{
+ 
+  int a = 10;
+  int b = 5;
+  int sum = a + b;             
+  float average = sum / 2.0;  
+  printf("Сумма: %d\n", sum);
+  printf("Среднее арифметическое: %f\n", average);
+ 
+  return 0;
+}
