c050b5b25d4f3f2b36074ed5f56d3bda37736cf6
AlexanderErmolov <amkof03@mail.ru>
2024-08-23 14:54:23 +0300

    Add files via upload

---
+++ Conditional_constructions_4.c
@@ -0,0 +1,29 @@
+/******************************************************************************
+
+Welcome to GDB Online.
+GDB online is an online compiler and debugger tool for C, C++, Python, Java, PHP, Ruby, Perl,
+C#, OCaml, VB, Swift, Pascal, Fortran, Haskell, Objective-C, Assembly, HTML, CSS, JS, SQLite, Prolog.
+Code, Compile, Run and Debug online from anywhere in world.
+
+*******************************************************************************/
+#include <stdio.h>
+
+int main(void)
+{
+    int x = 5;
+    switch(x)
+    {
+        case 4: 
+            printf("x = 4");
+            break;
+         
+        case 5: 
+            printf("x = 5");
+            break;
+         
+        case 6: 
+            printf("x = 6");
+            break;
+    }
+    return 0;
+}
